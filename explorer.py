5bcd936fa230919035b51cf39a1fd9c29a091fb3
gad-biton <gadb633@gmail.com>
2026-06-24 19:25:04 +0300

    fixed part_1 2

--- explorer.py
+++ explorer.py
@@ -4,5 +4,8 @@ fruits = ["apple", "banana", "cherry"]
 for fruit in fruits:
     print(fruit)
 # 2
-print(list(range(1,6)))
+for list in range(1,6):
+    print(list)
+# print(list(range(1,6)))
+# 3
     
